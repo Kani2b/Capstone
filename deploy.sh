f07a09b399ab11e67fabb9ebfdd7ef5f92063295
Kani2b <144917315+Kani2b@users.noreply.github.com>
2023-11-16 15:03:00 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-
-if [ "$CURRENT_BRANCH" == "dev" ]; then
+if [ "$CURRENT_BRANCH" == "origin/dev" ]; then
     ./build.sh
     docker tag react-app "kani2b/capstone-dev:dev"
     docker login -u kani2b -p dckr_pat_XQJzIzQcsy0RDmjGA1nOywp08IA
     docker push "kani2b/capstone-dev:dev"
     echo "Deployed to dev repository"
-elif [ "$CURRENT_BRANCH" == "master" ]; then
+elif [ "$CURRENT_BRANCH" == "origin/master" ]; then
     ./build.sh
     docker tag react-app "kani2b/capstone-prod:prod"
     docker login -u kani2b -p dckr_pat_XQJzIzQcsy0RDmjGA1nOywp08IA
